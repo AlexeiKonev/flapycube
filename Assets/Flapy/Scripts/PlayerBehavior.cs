1cf30d84e4befb5fd9897a03dc708144f82a28eb
AlexeiKonev <alexkonev777@gmail.com>
2023-08-07 14:35:43 +0300

    added gravitation

--- Assets/Flapy/Scripts/PlayerBehavior.cs
+++ Assets/Flapy/Scripts/PlayerBehavior.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// PlayerScript requires the GameObject to have a Rigidbody component
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Vector3 gravityDirection = Vector3.down;
+    [SerializeField] private Rigidbody playerRigidbody;
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GravityChange();
+    }
+
+    private void GravityChange()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gravityDirection == Vector3.down)
+            {
+                gravityDirection = Vector3.up;
+                playerRigidbody.velocity = gravityDirection;
+            }
+            else if (gravityDirection == Vector3.up)
+            {
+                gravityDirection = Vector3.down;
+                playerRigidbody.velocity = gravityDirection;
+            }
+
+            Debug.Log("presed");
+        }
     }
 }
